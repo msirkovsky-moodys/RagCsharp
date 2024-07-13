8562fc5e4e6f24f3d3ef5fff04bc9af5373da00e
Marek Sirkovský <marek.sirkovsky-non-empl@moodys.com>
2024-07-13 06:19:02 +0100

    chore

--- Web/review-sage-web/src/app/page.tsx
+++ Web/review-sage-web/src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import styles from "./page.module.css";
-import { RepoInput } from "./RepoInput";
+import { RepoInput } from "./repoInput";
 
 export async function fetchData() {
   const prNumber = -1;
